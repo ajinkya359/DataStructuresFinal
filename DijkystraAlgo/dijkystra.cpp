b6f5d69721f2d00f5c9d1ec6eaffcd9960b29000
ajinkya359 <sajinkya359@gmail.com>
2020-06-11 06:00:46 +0530

    Added path printing for the start and end.

--- DijkystraAlgo/dijkystra.cpp
+++ DijkystraAlgo/dijkystra.cpp
@@ -30,7 +30,27 @@ public:
     void add_directed_edge(int a,int b,int weight);
     void print_graph();
     void dijkstra(vertex* s);
+    void print_shortest_path(vertex* start,vertex* end);
+    void print_shortest_path_util(vertex* start,vertex* end);
 };
+void graph::print_shortest_path_util(vertex* start,vertex* end)
+{
+    if(start==end)
+        cout<<start->data<<endl;
+    else if(end->parent==NULL)
+        cout<<"No path exists"<<endl;
+    else
+    {
+        print_shortest_path_util(start,end->parent);
+        cout<<end->data<<endl;
+    }
+    
+}
+void graph::print_shortest_path(vertex* start,vertex* end)
+{
+    dijkstra(start);
+    print_shortest_path_util(start,end);
+}
 void graph::dijkstra(vertex* s)
 {
     for(int i=0;i<number_of_vertices;i++)
@@ -72,7 +92,7 @@ void graph::add_directed_edge(int a,int b,int weight){
     real_graph[a].neighbours.push_back(make_pair(&real_graph[b],weight));
 }
 int main(){
-    graph g(5);
+    graph g(7);
     g.add_directed_edge(0,1,10);
     g.add_directed_edge(0,4,5);
     g.add_directed_edge(1,2,1);
@@ -83,6 +103,8 @@ int main(){
     g.add_directed_edge(4,1,3);
     g.add_directed_edge(4,2,9);
     g.add_directed_edge(4,3,2);
+    //Vertex 6 and vertex 7 are in different component
+    g.add_directed_edge(6,7,1);
     g.dijkstra(&g.real_graph[0]);
-    cout<<g.real_graph[3].distance<<endl;
+    g.print_shortest_path(&g.real_graph[0],&g.real_graph[7]);
 }
